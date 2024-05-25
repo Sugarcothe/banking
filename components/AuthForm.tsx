4503057a9613dbd71daa9d446c5040c919df0fa3
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-05-25 10:42:55 +0100

    sign in and sign up data fetch implemented

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,6 +20,7 @@ import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 // const formSchema = z.object({
 //   email: z.string().email(),
@@ -27,6 +28,7 @@ import { Loader2 } from "lucide-react";
 // });
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter()
   const [user, setuser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -42,13 +44,28 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-    console.log(values);
-    setIsLoading(false);
-  }
+
+    try {
+      if (type === "sign-up") {
+        //  const newUser = await signUp(data)
+        //  setuser(newUser)
+      }
+       if (type === "sign-up") {
+        // const response = await signIn({
+        //   email: data.email,
+        //   password:data.password,
+        // })
+
+        // if(response) router.push("/")
+       }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <section className="auth-form">
@@ -97,6 +114,12 @@ const AuthForm = ({ type }: { type: string }) => {
                     control={form.control}
                     label="Address"
                     placeholder="Enter Your Address"
+                  />
+                  <CustomInput
+                    name="city"
+                    control={form.control}
+                    label="City"
+                    placeholder="Enter Your City"
                   />
                   <div className="flex gap-4 ">
                     <CustomInput
--- lib/utils.ts
+++ lib/utils.ts
@@ -195,17 +195,18 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authFormSchema = (type) =>
+export const authFormSchema = (type: string) =>
   z.object({
     // SIGN UP
-    firstName: type === "sign-in" ? z.string().optional : z.string().min(3),
-    lastName: type === "sign-in" ? z.string().optional : z.string().min(3),
-    address1: type === "sign-in" ? z.string().optional : z.string().max(50),
-    state: type === "sign-in" ? z.string().optional : z.string().min(3),
+    firstName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    address1: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    city: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    state: type === "sign-in" ? z.string().optional() : z.string().min(3),
     postalCode:
-      type === "sign-in" ? z.string().optional : z.string().min(3).max(7),
-    dateOfBirth: type === "sign-in" ? z.string().optional : z.string().min(3),
-    ssn: type === "sign-in" ? z.string().optional : z.string().min(3),
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(7),
+    dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    ssn: type === "sign-in" ? z.string().optional() : z.string().min(3),
 
     // SIGN IN & SIGN UP
     email: z.string().email(),
