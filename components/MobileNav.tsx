5165bcd2b3b9314672d47728f8e6aa3b0d586292
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-06-03 09:14:34 +0100

    header title fix

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -37,9 +37,9 @@ const MobileNav = ({ user }: MobileNavProps) => {
               src="/icons/logo.png"
               width={34}
               height={34}
-              alt="Horizon logo"
+              alt="WallBank logo"
             />
-            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">WallBank</h1>
           </Link>
           <div className="mobilenav-sheet">
             <SheetClose asChild>
